d944cb20cb99eda1544e2e5f45c35767a8015f46
marl0ny <52328879+marl0ny@users.noreply.github.com>
2021-04-05 20:32:28 -0400

    1D case and animations (no fancy transitions)

--- example.py
+++ example.py
@@ -1,13 +1,11 @@
 import numpy as np
-from qmsolve import Halmitonian, dynamic_visualize
-
-
-H = Halmitonian(N = 80, extent = 10)
-H.add_particle(spatial_ndim = 2)
-
+from qmsolve import Halmitonian, visualize, dynamic_visualize, animate
 
 L = 10
 N = 80
+spatial_ndim = 2
+
+
 x = np.linspace(-L/2,L/2,N)
 y = np.linspace(-L/2,L/2,N)
 xx, yy = np.meshgrid(x, y)
@@ -17,11 +15,15 @@ V = 200*(3-np.exp((-(xx)**2 -(yy-𝜇)**2 ) / (2*σ**2)) / (np.sqrt(2*np.pi)* σ
 -np.exp((-(xx-𝜇)**2 -(yy)**2 ) / (2*σ**2)) / (np.sqrt(2*np.pi)* σ)
 -np.exp((-(xx+𝜇)**2 -(yy)**2 ) / (2*σ**2)) / (np.sqrt(2*np.pi)* σ)
 -np.exp((-(xx)**2 -(yy+𝜇)**2 ) / (2*σ**2)) / (np.sqrt(2*np.pi)* σ))
+# V = 200*x**2
 
 
+H = Halmitonian(N = N, extent = L)
+H.add_particle(spatial_ndim = spatial_ndim)
 H.add_potential(V)
+
 energies, eigenstates = H.solve(30)
 
 
 #visualize(energies, eigenstates, 23) #static version, only plots a eigenstate
-dynamic_visualize(energies, eigenstates)
+animate(energies, eigenstates)
--- qmsolve/visualization.py
+++ qmsolve/visualization.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 
 def visualize(energies, eigenstates, k):
+
+    ndim = len(eigenstates[k].shape)
     # 2d prototype for static visualization.  This should handle several the other dimensions
 
     fig = plt.figure(figsize=(16/9 *5.804 * 0.9,5.804)) 
@@ -11,8 +13,6 @@ def visualize(energies, eigenstates, k):
     ax1 = fig.add_subplot(grid[0:2, 0:1])
     ax2 = fig.add_subplot(grid[0:2, 1:2])
 
-    ax1.set_aspect('equal')
-    ax1.set_ylabel("[Å]")
     ax1.set_xlabel("[Å]")
 
     ax2.set_title('E Level')
@@ -26,8 +26,13 @@ def visualize(energies, eigenstates, k):
     for E in energies:
         ax2.plot([0,1], [E/E0, E/E0], color='gray', alpha=0.5)
 
-        
-    im = ax1.imshow((eigenstates[k]), cmap ='seismic',  interpolation = 'bilinear')
+    if ndim == 2:
+        ax1.set_aspect('equal')
+        ax1.set_ylabel("[Å]")
+        im = ax1.imshow((eigenstates[k]), cmap ='seismic',  interpolation = 'bilinear')
+    else:
+        ax1.plot(eigenstates[k])
+
     ax2.plot([0,1], [energies[k]/E0, energies[k]/E0], color='yellow', lw = 3)
     plt.show()
 
@@ -36,16 +41,15 @@ def visualize(energies, eigenstates, k):
 
 
 def dynamic_visualize(energies, eigenstates):
-    # 2d slider prototype. This should handle several the other dimensions
+    # 1d and 2d slider prototype. This should handle several the other dimensions
 
+    ndim = len(eigenstates[0].shape)
     fig = plt.figure(figsize=(16/9 *5.804 * 0.9,5.804)) 
 
     grid = plt.GridSpec(2, 2, width_ratios=[5, 1], height_ratios=[1, 1] , hspace=0.1, wspace=0.2)
     ax1 = fig.add_subplot(grid[0:2, 0:1])
     ax2 = fig.add_subplot(grid[0:2, 1:2])
 
-    ax1.set_aspect('equal')
-    ax1.set_ylabel("[Å]")
     ax1.set_xlabel("[Å]")
 
     ax2.set_title('E Level')
@@ -58,8 +62,17 @@ def dynamic_visualize(energies, eigenstates):
     E0 = energies[0]
     for E in energies:
         ax2.plot([0,1], [E/E0, E/E0], color='gray', alpha=0.5)
+    
+    if ndim == 2:
+        ax1.set_aspect('equal')
+        ax1.set_ylabel("[Å]")
+        eigenstate_plot = ax1.imshow((eigenstates[1]), cmap ='seismic',  
+                                     interpolation = 'bilinear')
+    elif ndim == 1:
+        # ax1.set_xlim( ??? )
+        eigenstate_plot, = ax1.plot(eigenstates[1])
+        eigenstate_plot.set_data = eigenstate_plot.set_ydata
 
-    im = ax1.imshow((eigenstates[1]), cmap ='seismic',  interpolation = 'bilinear')
     line = ax2.plot([0,1], [energies[1]/E0, energies[1]/E0], color='yellow', lw = 3)
 
     plt.subplots_adjust(bottom=0.2)
@@ -75,7 +88,7 @@ def dynamic_visualize(energies, eigenstates):
                      )
 
     def update(state):
-        im.set_data(eigenstates[state])
+        eigenstate_plot.set_data(eigenstates[state])
         line[0].set_ydata([energies[state]/E0, energies[state]/E0])
 
     slider.on_changed(update)
@@ -85,4 +98,51 @@ def dynamic_visualize(energies, eigenstates):
 
 
 def animate(energies, eigenstates):
-    raise NotImplementedError('animate')
+
+    ndim = len(eigenstates[0].shape)
+    fig = plt.figure(figsize=(16/9 *5.804 * 0.9,5.804)) 
+
+    grid = plt.GridSpec(2, 2, width_ratios=[5, 1], height_ratios=[1, 1] , hspace=0.1, wspace=0.2)
+    ax1 = fig.add_subplot(grid[0:2, 0:1])
+    ax2 = fig.add_subplot(grid[0:2, 1:2])
+
+    ax1.set_xlabel("[Å]")
+
+    ax2.set_title('E Level')
+    ax2.set_facecolor('black')
+
+    ax2.set_ylabel('$E_N$ (Relative to $E_{1}$)')
+    ax2.set_xticks(ticks=[])
+
+
+    E0 = energies[0]
+    for E in energies:
+        ax2.plot([0,1], [E/E0, E/E0], color='gray', alpha=0.5)
+    
+    if ndim == 2:
+        ax1.set_aspect('equal')
+        ax1.set_ylabel("[Å]")
+        eigenstate_plot = ax1.imshow((eigenstates[1]), cmap ='seismic',  
+                                     interpolation = 'bilinear')
+    elif ndim == 1:
+        # ax1.set_xlim( ??? )
+        eigenstate_plot, = ax1.plot(eigenstates[1])
+        eigenstate_plot.set_data = eigenstate_plot.set_ydata
+
+    line, = ax2.plot([0,1], [energies[1]/E0, energies[1]/E0], color='yellow', lw = 3)
+
+    plt.subplots_adjust(bottom=0.2)
+
+    import matplotlib.animation as animation
+
+    animation_data = {'n': 0.0}
+    def func_animation(*arg):
+        animation_data['n'] = (animation_data['n'] + 0.1) % len(energies)
+        state = int(animation_data['n'])
+        eigenstate_plot.set_data(eigenstates[int(state)])
+        line.set_ydata([energies[int(state)]/E0, energies[int(state)]/E0])
+        return eigenstate_plot, line
+
+    a = animation.FuncAnimation(fig, func_animation,
+                                blit=True, interval=1.0)
+    plt.show()
