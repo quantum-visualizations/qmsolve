65795f5a59dc9f3a1606ec7b7db34285a64f271b
Hudson <dhudsmith@gmail.com>
2021-04-30 21:09:43 -0400

    Initial port of lobpcg

--- qmsolve/hamiltonian.py
+++ qmsolve/hamiltonian.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.sparse.linalg import eigsh
+from scipy.sparse.linalg import eigsh, lobpcg, LinearOperator
 from scipy.sparse import diags
 import time
 
@@ -30,16 +30,47 @@ class Hamiltonian:
         V = diags([V], [0])
         return V
 
-    def solve(self, max_states):
+    def solve(self, max_states: int, method: str = 'eigsh'):
+        """
+        Diagonalize the hamiltonian and retrieve the lowest-energy eigenstates
+        Args:
+            max_states: the number of states to retreive
+            method: the solver method. Currently, 'eigsh' and 'lobpcg' are implemented. Note: 'lobpcg' is potentially
+            much faster than 'eigsh' but can fail catastrophically for some systems. Use 'lobpcg' with care.
+
+        Returns:
+
+        """
+        implemented_solvers = ('eigsh', 'lobpcg')
+
         H = self.T + self.V
         print("Computing...")
         t0 = time.time()
-        eigenvalues, eigenvectors = eigsh(H, k=max_states, which='LM', sigma=0)
 
-        """
-        the result of this method depends of the particle system. For example if the systems are two fermions, this method
-        makes the eigenstates antisymmetric
-        """
+        if method == 'eigsh':
+            # Note: uses shift-invert trick for stability finding low-lying states
+            # Ref: https://docs.scipy.org/doc/scipy/reference/tutorial/arpack.html#shift-invert-mode
+            eigenvalues, eigenvectors = eigsh(H, k=max_states, which='LM', sigma=0)
+        elif method == 'lobpcg':
+            # preconditioning matrix should approximate the inverse of the hamiltonian
+            # we naively construct this by taking the inverse of diagonal elements
+            # and setting all others to zero. This is called the Jacobi or diagonal preconditioner.
+            A = diags([1 / H.diagonal()], [0]).tocsc()
+            precond = lambda x: A @ x
+            M = LinearOperator(H.shape, matvec=precond, matmat=precond)
+
+            # guess for eigenvectors is computed from random numbers
+            # TODO: a better guess would be to use the eigenstates of a reference system like a square well
+            X_approx = np.random.rand(H.shape[0], max_states)
+
+            sol = lobpcg(H, X_approx, largest=False, M=M, tol=1e-15)
+            eigenvalues, eigenvectors = sol[0], sol[1]
+        else:
+            raise NotImplementedError(
+                f"{method} solver has not been implemented. Use one of {implemented_solvers}")
+
+        """the result of this method depends of the particle system. For example if the systems are two fermions, 
+        this method makes the eigenstates antisymmetric """
         self.eigenstates = self.particle_system.get_eigenstates(self, max_states, eigenvalues, eigenvectors)
 
         print("Took", time.time() - t0)
