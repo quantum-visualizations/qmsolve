c1a0ff039ebe8e25f6e5c886550b238ad85db1a3
Rafael de la Fuente <rafael.fuente.herrezuelo@gmail.com>
2021-05-04 17:04:40 +0200

    add 3d density plot animations and fix two particles @abstractmethod bug

--- 3D_harmonic_oscillator.py
+++ 3D_harmonic_oscillator.py
@@ -3,40 +3,23 @@ from qmsolve import visualization
 from qmsolve import Hamiltonian, SingleParticle, Eigenstates
 
 
-spatial_ndim = 1
-L = 10
-N = 100
-separable_potential = [lambda particle: 20.0*particle.x**2,
-                       lambda particle: 20.0*particle.x**2,
-                       lambda particle: 20.0*particle.x**2]
+#interaction potential
+def harmonic_oscillator(particle):
 
-energies = []
-states = []
-M = 3 # Number of 1D eigenstates to solve.
-
-for V_i in separable_potential:
-    H_i = Hamiltonian(particles=SingleParticle(),
-                      N=N, potential=V_i, 
-                      extent=L, spatial_ndim=1)
-    v = H_i.solve(M)
-    energies.append(v.energies)
-    states.append(v.array)
-
-states = np.multiply.outer(np.multiply.outer(states[2], states[1]), states[0])
-states = np.transpose(states, (0, 2, 4, 1, 3, 5))
-energies = np.add.outer(np.add.outer(energies[2], energies[1]), energies[0])
-energies = energies.flatten()
-states = states.reshape([M*M*M, N, N, N])
-sort_array = np.argsort(energies)
-# How to rearrange an array with argsort output:
-# https://stackoverflow.com/a/64638032
-energies = energies[sort_array]
-states = states[sort_array]
+	kx, ky, kz = [2]*3 # measured in eV / (Å**2)
+	return 0.500 * kx * particle.x**2 +\
+		   0.51 * ky * particle.y**2 +\
+		   0.52 * kz * particle.z**2
 
 
-eigenstates = Eigenstates(energies, states, H_i, 'SingleParticle3D')
-v = visualization.init_visualization(eigenstates)
-v.plot_eigenstate(26)
-v.plot_type = 'contour'
-v.plot_eigenstate(26)
-v.animate()
+H = Hamiltonian(particles=SingleParticle(),
+                  N=30, potential=harmonic_oscillator, 
+                  extent=10, spatial_ndim=3)
+
+
+eigenstates = H.solve(max_states = 32)
+print(eigenstates.energies)
+
+visualization = visualization.init_visualization(eigenstates)
+visualization.plot_eigenstate(26)
+visualization.animate()
--- qmsolve/visualization/single_particle_3D.py
+++ qmsolve/visualization/single_particle_3D.py
@@ -15,14 +15,25 @@ class VisualizationSingleParticle3D(Visualization):
     def plot_eigenstate(self, k):
         eigenstates = self.eigenstates.array
         mlab.figure(1, bgcolor=(0, 0, 0), size=(350, 350))
-        psi2 = np.abs(eigenstates[k])
-        min = psi2.min()
-        max = psi2.max()
+        psi = np.abs(eigenstates[k])
+        min = psi.min()
+        max = psi.max()
+
+
+
         if self.plot_type == 'volume':
 
-            vol = mlab.pipeline.volume(mlab.pipeline.scalar_field(psi2), 
+            L = self.eigenstates.extent/2
+            N = self.eigenstates.N
+
+            xx, yy, zz = np.mgrid[-L:L:N*1j, -L:L:N*1j, -L:L:N*1j]
+            vol = mlab.pipeline.volume(mlab.pipeline.scalar_field(xx,yy,zz,psi), 
                                     vmin=min + 0.008 * (max - min),        
                                     vmax=min + 0.5 * (max - min))
+
+            mlab.outline()
+
+            mlab.axes(xlabel='x [Å]', ylabel='y [Å]', zlabel='z [Å]',nb_labels=4)
 
         else:
 
@@ -32,7 +43,7 @@ class VisualizationSingleParticle3D(Visualization):
             field = mlab.pipeline.scalar_field(psi2, 
                                             vmin=min + 0.008 * (max - min),
                                             vmax=min + 0.5 * (max - min))
-            field.image_data.point_data.add_array(eigenstates[k].T.ravel())
+            field.image_data.point_data.add_array(eigenstates[k].ravel())
             field.image_data.point_data.get_array(1).name = 'phase'
             field.update()
             field2 = mlab.pipeline.set_active_attribute(field, 
@@ -42,45 +53,93 @@ class VisualizationSingleParticle3D(Visualization):
                                                         point_scalars='phase')
             mlab.pipeline.surface(contour2)
 
-        mlab.outline()
-        mlab.axes(nb_labels=4)
-
-
-        φ = 30
-        mlab.view(azimuth= φ)
-
         mlab.show()
 
     def animate(self):
         eigenstates = self.eigenstates.array
         energies = self.eigenstates.energies
         mlab.figure(1, bgcolor=(0, 0, 0), size=(350, 350))
-        field = mlab.pipeline.scalar_field(np.abs(eigenstates[0]))
-        colour_data = np.angle(eigenstates[0].T.ravel())
-        field.image_data.point_data.add_array(colour_data)
-        field.image_data.point_data.get_array(1).name = 'phase'
-        field.update()
-        field2 = mlab.pipeline.set_active_attribute(field, 
-                                                    point_scalars='scalar')
-        contour = mlab.pipeline.contour(field2)
-        contour2 = mlab.pipeline.set_active_attribute(contour, 
-                                                    point_scalars='phase')
-        mlab.pipeline.surface(contour2)
-        data = {'t': 0.0}
-        @mlab.animate(delay=10)
-        def animation():
-            while (1):
-                data['t'] += 0.05
-                k1 = int(data['t']) % len(energies)
-                k2 = (int(data['t']) + 1) % len(energies)
-                if data['t'] % 1.0 > 0.5:
-                    t = (data['t'] - int(data['t']) - 0.5)
-                    psi = (np.cos(np.pi*t)*eigenstates[k1]
-                        + np.sin(np.pi*t)*eigenstates[k2])
-                else:
-                    psi = eigenstates[k1]
-                np.copyto(colour_data, np.angle(psi.T.ravel()))
-                field.mlab_source.scalars = np.abs(psi)
-                yield
-        animation()
-        mlab.show()
+
+
+        if self.plot_type == 'volume':
+            L = self.eigenstates.extent/2
+            N = self.eigenstates.N
+
+            xx, yy, zz = np.mgrid[-L:L:N*1j, -L:L:N*1j, -L:L:N*1j]
+            psi = np.abs(eigenstates[0])
+            min = np.amin(np.abs(eigenstates[:]))
+            max = np.amax(np.abs(eigenstates[:]))
+
+            vmin = min + 0.008 * (max - min)
+            vmax = min + 0.4 * (max - min)
+            psi = np.where(psi > vmax, vmax,psi)
+            psi = np.where(psi < vmin, vmin,psi)
+
+            field = mlab.pipeline.scalar_field(xx, yy, zz, psi)
+            vol = mlab.pipeline.volume(field)
+            φ = 30
+            mlab.view(azimuth= φ, distance=L*6.7)
+            mlab.outline()
+
+            mlab.axes(xlabel='x [Å]', ylabel='y [Å]', zlabel='z [Å]',nb_labels=4)
+
+            data = {'t': 0.0}
+            @mlab.animate(delay=10)
+            def animation():
+                while (1):
+                    data['t'] += 0.05
+                    k1 = int(data['t']) % len(energies)
+                    k2 = (int(data['t']) + 1) % len(energies)
+                    if data['t'] % 1.0 > 0.5:
+                        t = (data['t'] - int(data['t']) - 0.5)
+                        psi = (np.cos(np.pi*t)*eigenstates[k1]
+                            + np.sin(np.pi*t)*eigenstates[k2])
+                    else:
+                        psi = eigenstates[k1]
+
+                    psi = np.abs(psi)
+                    vmin = min + 0.008 * (max - min)
+                    vmax = min + 0.4 * (max - min)
+                    psi = np.where(psi > vmax, vmax,psi)
+                    psi = np.where(psi < vmin, vmin,psi)
+
+                    field.mlab_source.scalars = np.abs(psi)
+                    φ = 30 + data['t'] * 360 / 10 
+                    mlab.view(azimuth= φ, distance=L*6.7)
+
+                    yield
+
+            animation()
+            mlab.show()
+
+        else:
+
+            field = mlab.pipeline.scalar_field(np.abs(eigenstates[0]))
+            colour_data = np.angle(eigenstates[0].ravel())
+            field.image_data.point_data.add_array(colour_data)
+            field.image_data.point_data.get_array(1).name = 'phase'
+            field.update()
+            field2 = mlab.pipeline.set_active_attribute(field, 
+                                                        point_scalars='scalar')
+            contour = mlab.pipeline.contour(field2)
+            contour2 = mlab.pipeline.set_active_attribute(contour, 
+                                                        point_scalars='phase')
+            mlab.pipeline.surface(contour2)
+            data = {'t': 0.0}
+            @mlab.animate(delay=10)
+            def animation():
+                while (1):
+                    data['t'] += 0.05
+                    k1 = int(data['t']) % len(energies)
+                    k2 = (int(data['t']) + 1) % len(energies)
+                    if data['t'] % 1.0 > 0.5:
+                        t = (data['t'] - int(data['t']) - 0.5)
+                        psi = (np.cos(np.pi*t)*eigenstates[k1]
+                            + np.sin(np.pi*t)*eigenstates[k2])
+                    else:
+                        psi = eigenstates[k1]
+                    np.copyto(colour_data, np.angle(psi.T.ravel()))
+                    field.mlab_source.scalars = np.abs(psi)
+                    yield
+            animation()
+            mlab.show()
